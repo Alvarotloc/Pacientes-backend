20b567b938a03154c4888b32d07934e1256e52bd
Alvarotloc <alvarotloc@gmail.com>
2022-05-23 20:18:59 +0200

    Nuevo proyecto

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '05bff09d680c974227cf81ae26f9229a'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e6524370-9dfd-4595-979c-df266da855ac'
+};
